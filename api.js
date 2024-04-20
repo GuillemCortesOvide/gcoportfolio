8599c03acd0a6c5e75d8c4c3d6f0cbab762c44b7
Guillem Cortes <guillem.cortes@gfk.com>
2024-04-20 11:41:13 +0200

    Add api.js

---
+++ api.js
@@ -0,0 +1,17 @@
+// api.js
+
+// Function to fetch follower count from YouTube API
+async function getYouTubeSubscribers() {
+    const response = await fetch('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=gc_tech&key=YOUR_API_KEY');
+    const data = await response.json();
+    return data.items[0].statistics.subscriberCount;
+}
+
+// Function to update HTML with follower counts
+async function updateFollowers() {
+    const youtubeSubscribers = await getYouTubeSubscribers();
+    document.getElementById('youtube-subscribers').innerText = youtubeSubscribers;
+}
+
+// Call the updateFollowers function to fetch and update follower counts
+updateFollowers();
